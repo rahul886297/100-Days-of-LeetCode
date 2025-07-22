c34d94e68baf1be8049fd3df33bc1f8e3d351ba4
Rahul Mandal <126048367+rahul886297@users.noreply.github.com>
2025-07-22 22:19:55 +0530

    Time: 0 ms (100%), Space: 41.7 MB (13.47%) - LeetHub

---
+++ 1114-binary-search-tree-to-greater-sum-tree/1114-binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,17 @@
+class Solution {
+    int sum = 0 ;
+    public TreeNode bstToGst(TreeNode root) {
+        helper(root);
+        return root ;
+    }
+
+    public void helper(TreeNode root){
+        if(root == null){
+            return ;
+        }
+        helper(root.right);
+        sum = sum + root.val;
+        root.val = sum ;
+        helper(root.left);
+    }
+}
